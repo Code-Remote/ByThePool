3b86ec81be448eee51f8631196c0c89d90033846
Code-Remote <evert@code-remote.com>
2020-07-16 15:49:46 +0200

    Basic layout lessons

--- app/src/main/java/com/code_remote/bythepool/MainActivity.kt
+++ app/src/main/java/com/code_remote/bythepool/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val x = 20
         setContentView(R.layout.activity_main)
     }
 }

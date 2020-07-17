37098c4de668288d7e3474d7b6e7f85def2a95fe
Code-Remote <evert@code-remote.com>
2020-07-17 21:54:53 +0200

    All you need for Firebase App messaging. google-services.json is in the .gitignore download from your firebase project's console

---
+++ app/src/main/java/com/code_remote/bythepool/MyFirebaseMessagingService.kt
@@ -0,0 +1,42 @@
+package com.code_remote.bythepool
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    private val TAG: String = "MyFcmService"
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // ...
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.e(TAG, "From: " + remoteMessage.from)
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+            if ( /* Check if data needs to be processed by long running job */true) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+
+            } else {
+                // Handle message within 10 seconds
+            }
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.notification != null) {
+            Log.e(TAG, "Message Notification Body: " + remoteMessage.notification!!.body)
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+
+    override fun onNewToken(newFcmToken: String?) {
+        super.onNewToken(newFcmToken)
+        Log.e(TAG, "My new token $newFcmToken")
+    }
+}
